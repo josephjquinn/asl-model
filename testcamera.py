0a98a515e9506b2b75b03e95d06482e7cd89abcb
Joseph Quinn <81782398+josephjquinn@users.noreply.github.com>
2024-02-17 16:27:10 -0600

    adding test camera script

---
+++ testcamera.py
@@ -0,0 +1,9 @@
+import cv2
+from cvzone.HandTrackingModule import HandDetector
+cap = cv2.VideoCapture(0)
+detector = HandDetector(maxHands=2)
+while True:
+    success, img = cap.read()
+    hands, img = detector.findHands(img)
+    cv2.imshow("Result", img)
+    cv2.waitKey(1)
